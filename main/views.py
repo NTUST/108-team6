fbc8069a73e85c3f79038250b42f01229ff4a012
Leo <karta09104@gmail.com>
2019-06-05 11:08:02 +0800

    players order

--- main/views.py
+++ main/views.py
@@ -15,12 +15,31 @@ def index(request):
 
 @require_http_methods(["GET"])
 def players(request):
+    parameter = {}
     page = int(request.GET.get('page', 1))
     page = page if page > 1 else 1
     limit = int(request.GET.get('limit', 20))
     limit = limit if 0 < limit < 50 else 20
     start = (page - 1) * limit
-    player_list = Player.objects.all()[start:start + limit]
+
+    player_list = Player.objects.filter()
+    order = request.GET.get("order")
+    order_by = request.GET.get("order_by")
+    search_name = request.GET.get("search")
+    if search_name:
+        parameter["search"] = search_name
+        player_list = player_list.filter(name__contains=search_name)
+    if order and order_by:
+        order_column = order_by
+        if order not in ["asc", "desc"]:
+            order = "asc"
+        if order == "desc":
+            order_column = f"-{order_column}"
+        # parameter["order"] = order
+        # parameter["order_by"] = order_by
+        player_list = player_list.order_by(order_column)
+    player_list = player_list.all()[start:start + limit]
+    query_string = parse_query_string(parameter=parameter)
     return render(request, "players.html", locals())
 
 
@@ -31,9 +50,14 @@ def refresh_data(request):
     return HttpResponse(status=200)
 
 
+def parse_query_string(parameter):
+    query_string = "&".join([f"{i}={v}" for i, v in parameter.items()])
+    return f"?{query_string}&"
+
+
 def get_player(request, player_name):
     try:
-        player = Player.objects.get(name=player_name)
+        player = Player.objects.get(id=player_name)
     except Player.DoesNotExist:
         player = None
 
