2290f961b4a4690b680cfada4079596e1be29a54
amura87516 <stevenlinson168@gmail.com>
2019-06-12 09:30:11 +0800

    修改model

--- analysis/views.py
+++ analysis/views.py
@@ -8,32 +8,27 @@ from django.views.decorators.csrf import csrf_exempt
 from keras import losses
 from keras import optimizers
 from keras import regularizers
-from keras.layers import BatchNormalization, Dense, LeakyReLU
+from keras.layers import BatchNormalization, Dense, LeakyReLU, Input
 from keras.models import Sequential
+import keras
 
-
-# Create your views here.
+def SeperateModel(neuron_num, input_layer1):
+    
+    dense = Dense(neuron_num,kernel_initializer='random_uniform')(input_layer1)
+    BN = BatchNormalization()(dense)
+    leakyReLU = LeakyReLU(alpha=.001)(BN)
+    return leakyReLU
 
 def Model(attribute_num):
-    model = Sequential()
-    model.add(Dense(512,kernel_initializer='random_uniform', input_shape=(attribute_num,), activity_regularizer=regularizers.l2(0.001)))
-    model.add(BatchNormalization())
-    # model.add(Activation('relu'))
-    model.add(LeakyReLU(alpha=.001))
-    model.add(Dense(256, kernel_initializer='random_uniform', activity_regularizer=regularizers.l2(0.001)))
-    model.add(BatchNormalization())
-    # model.add(Activation('relu'))
-    model.add(LeakyReLU(alpha=.001))
-    model.add(Dense(128, kernel_initializer='random_uniform', activity_regularizer=regularizers.l2(0.001)))
-    model.add(BatchNormalization())
-    # model.add(Activation('relu'))
-    model.add(LeakyReLU(alpha=.001))
-    model.add(Dense(64, kernel_initializer='random_uniform', activity_regularizer=regularizers.l2(0.001)))
-    model.add(BatchNormalization())
-    model.add(LeakyReLU(alpha=.001))
-    # model.add(Activation('relu'))
-    model.add(Dense(1))
-
+    input_layer = Input(shape=(attribute_num,))
+    BN = BatchNormalization()(input_layer)
+    dnn = SeperateModel(128,BN)
+    dnn = SeperateModel(128,dnn)
+    # dnn = SeperateModel(256,dnn)
+    # dnn = SeperateModel(256,dnn)
+    outs = Dense(1,kernel_initializer='random_uniform')(dnn)
+    
+    model = keras.models.Model(inputs=input_layer, outputs=outs)
     model.compile(optimizer=optimizers.Adam(lr=1e-2),
                   loss=losses.mean_absolute_error)
     return model
@@ -43,7 +38,7 @@ def predict(request):
 
 @csrf_exempt
 def predict_result(request):
-    arr_list = ['Age', 'Overall', 'Potential', 'Special', 'Acceleration', 'Aggression', 'Agility', 'Balance', 'Ball Control', 'Composure', 'Crossing', 'Curve', 'Dribbling', 'Finishing', 'Free Kick Accuracy', 'GK Diving', 'GK Handling', 'GK Kicking', 'GK Positioning', 'GK Reflexes', 'Heading Accuracy', 'Interceptions', 'Jumping', 'Long Passing', 'Long Shots', 'Marking', 'Penalties', 'Positioning', 'Reactions', 'Short Passing', 'Shot Power', 'Sliding Tackle', 'Sprint Speed', 'Stamina', 'Standing Tackle', 'Strength', 'Vision', 'Volleys']
+    arr_list = ['Age','Special', 'Acceleration', 'Aggression', 'Agility', 'Balance', 'Ball Control', 'Composure', 'Crossing', 'Curve', 'Dribbling', 'Finishing', 'Free Kick Accuracy', 'GK Diving', 'GK Handling', 'GK Kicking', 'GK Positioning', 'GK Reflexes', 'Heading Accuracy', 'Interceptions', 'Jumping', 'Long Passing', 'Long Shots', 'Marking', 'Penalties', 'Positioning', 'Reactions', 'Short Passing', 'Shot Power', 'Sliding Tackle', 'Sprint Speed', 'Stamina', 'Standing Tackle', 'Strength', 'Vision', 'Volleys']
     prefer_list = ['CAM', 'CB',  'CDM',  'CF',  'CM',  'GK',  'LB',  'LM',  'LW',  'RB',  'RM',  'RW',  'RWB',  'ST','LWB']
 
     inputs = []
@@ -67,6 +62,6 @@ def predict_result(request):
 #     import psutil
 #     process = psutil.Process(os.getpid())
 #     print(process.memory_info().rss/786/1000000,' Mb')  # in bytes 
-    return HttpResponse(json.dumps({'wage':wage,'title':'Analysis'}), content_type='application/json')
+    return HttpResponse(json.dumps({'wage':wage}), content_type='application/json')
 
 
